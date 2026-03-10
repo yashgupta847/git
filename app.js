a8ad9e7b7204cd068af8919a7aadd5801e01dbc4
Yash Gupta <Yash Gupta>
2026-03-10 22:16:44 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-// server.maxRequestsPerSocket = 1;
+//server.maxRequestsPerSocket = 1;
 app.use(express.static("public"));
-
+console.log("Hello");
 app.listen(3000, (req, res) => {
   console.log("I am Listening");
 });
